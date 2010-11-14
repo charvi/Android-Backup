ffcddf14c8ad489db83f5c1cc9a64e254b90027e
charvi <charvipuri@gmail.com>
2010-11-14 19:10:39 +0530

    trying to display sms using array adapter ... still not working :(

--- src/android/backup/screens/SmsScreen.java
+++ src/android/backup/screens/SmsScreen.java
@@ -5,19 +5,25 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import android.app.Activity;
+
+import android.app.ListActivity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-public class SmsScreen extends Activity {
+public class SmsScreen extends ListActivity 
+{
 
 	String extStorageDirectory ;
+	String[] data =  new String[10];
+	String[] sender =  new String[10];
 
-	public void onCreate(Bundle savedInstanceState) {    
-		super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) 
+	{    
+		super.onCreate(savedInstanceState);	
 		try {
 			smsDisplay();
 		} catch (FileNotFoundException e) {
@@ -26,14 +32,16 @@ public class SmsScreen extends Activity {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}       
+		}
+
+		//		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.sms, data);
+		//		setListAdapter(arrayAdapter);
 	}
 
-	private void smsDisplay() throws IOException {		
-		String[] data=new String[100];
-		String[] sender=new String[100];
-
-		for(int i=0;i<100;i++)
+	private void smsDisplay() throws IOException
+	{
+		
+		for(int i=0;i<10;i++)
 		{
 			data[i]="null";
 		}
@@ -42,7 +50,7 @@ public class SmsScreen extends Activity {
 
 		Cursor SMScursor=this.managedQuery(mSmsQueryUri,new String[] { "_id", "thread_id", "address", "person", "date", "body" },null,null,null);
 		startManagingCursor(SMScursor);
-				
+
 		extStorageDirectory = Environment.getExternalStorageDirectory().toString();
 		Toast.makeText(this, extStorageDirectory, Toast.LENGTH_LONG);
 		OutputStream outStream = null;
@@ -64,30 +72,25 @@ public class SmsScreen extends Activity {
 
 					data[i]=body;
 					sender[i]=address;             
-					//osw.write(data[i]);
+
 					byte []buffer = data[i].getBytes();
 					outStream.write(buffer);
-
-
+					
 					Toast.makeText(this,address, Toast.LENGTH_LONG).show();
 					Toast.makeText(this,body, Toast.LENGTH_LONG).show();
-
-				}       
-
+				}
 			}
 			else
 			{
 				Toast.makeText(this,"NO MESSAGES IN INBOX",Toast.LENGTH_LONG).show();
 			}
-
-//			osw.flush();
-	//		osw.close();
 			outStream.flush();
 			outStream.close();
 			SMScursor.close();
-
-
 		}
 
-	}   
+		//	ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.sms, data);
+		//	setListAdapter(arrayAdapter);
+
+	}
 }
